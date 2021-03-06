ccd5b95702dab3b36fb2224da01518189bc2555a
Prokhor Proshkin <pproshkin@yandex.ru>
2021-03-06 09:56:08 +0300

    !!!

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         Random rnd = new Random();
-        int [][] arr = new int [10][10];
+        int [][] arr = new int [3][3];
 
         for (int i = 0; i < arr.length; i++ ){
             for (int j = 0; j < arr.length; j++){
@@ -22,6 +22,38 @@ public class Main {
        for (int [] a: arr ){
            System.out.println(Arrays.toString(a));
        }
+
+        int min = 0;
+        int max = 0;
+        int sumMin = 0;
+        int sumMax = 0;
+
+        for(int i=0; i<arr.length;i++){
+            sumMin += arr[i][0];
+        }
+            sumMax=sumMin;
+
+        System.out.println(sumMin);
+
+        for (int j=0; j< arr.length; j++){
+            int sum =0;
+            for(int i=0; i< arr.length;i++){
+            sum += arr[i][j];
+            }
+
+            if(sum>sumMax){
+                sumMax = sum;
+                max = j;
+            }
+
+            if (sum<sumMin){
+                sumMin = sum;
+                min =j;
+            }
+
+            System.out.println(sumMax);
+            System.out.println(sumMin);
+        }
 
 
 
